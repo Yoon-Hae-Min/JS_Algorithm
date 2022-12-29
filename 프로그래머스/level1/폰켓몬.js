5520add5e708bbd5130c504df790a258300beecf
Yoon-Hae-Min <yunhatmi@naver.com>
2022-12-29 11:17:53 +0900

    level1: 폰켓몬 https://school.programmers.co.kr/learn/courses/30/lessons/1845

---
+++ 프로그래머스/level1/폰켓몬.js
@@ -0,0 +1,6 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+function solution(nums) {
+  const set = new Set(nums);
+  return set.size > nums.length / 2 ? nums.length / 2 : set.size;
+}
